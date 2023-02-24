a24bdfe84565439c78552610f6590fa35fb7d2f1
Ruthvick2323 <125614920+Ruthvick2323@users.noreply.github.com>
2023-02-24 12:35:28 -0600

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -1,7 +1,52 @@
-const { v1: Uuidv1 } = require('uuid');
-//const JWT = require('../utils/jwtDecoder');
-//const SFClient = require('../utils/sfmc-client');
-//const logger = require('../utils/logger');
+'use strict';
+var util = require('util');
+
+// Deps
+const Path = require('path');
+//const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
+var util = require('util');
+var http = require('https');
+
+exports.logExecuteData = [];
+
+function logData(req) {
+    exports.logExecuteData.push({
+        body: req.body,
+        headers: req.headers,
+        trailers: req.trailers,
+        method: req.method,
+        url: req.url,
+        params: req.params,
+        query: req.query,
+        route: req.route,
+        cookies: req.cookies,
+        ip: req.ip,
+        path: req.path,
+        host: req.host,
+        fresh: req.fresh,
+        stale: req.stale,
+        protocol: req.protocol,
+        secure: req.secure,
+        originalUrl: req.originalUrl
+    });
+    console.log("body: " + util.inspect(req.body));
+    console.log("headers: " + req.headers);
+    console.log("trailers: " + req.trailers);
+    console.log("method: " + req.method);
+    console.log("url: " + req.url);
+    console.log("params: " + util.inspect(req.params));
+    console.log("query: " + util.inspect(req.query));
+    console.log("route: " + req.route);
+    console.log("cookies: " + req.cookies);
+    console.log("ip: " + req.ip);
+    console.log("path: " + req.path);
+    console.log("host: " + req.host);
+    console.log("fresh: " + req.fresh);
+    console.log("stale: " + req.stale);
+    console.log("protocol: " + req.protocol);
+    console.log("secure: " + req.secure);
+    console.log("originalUrl: " + req.originalUrl);
+}
 
 /**
  * The Journey Builder calls this method for each contact processed by the journey.
